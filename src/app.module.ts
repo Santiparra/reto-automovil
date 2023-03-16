6b9a8c62d701e1f8685fca7a69389df3fe0ab410
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-03-16 18:43:04 -0300

    añadido el module config y configurada la db mediante env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,15 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AutomovilModule } from './App-Local-Version-Buena/automovil/automovil.module';
 import { AutomovilMysqlModule } from './App-Mysql/automovil-mysql/automovil-mysql.module';
 import { LoggerModule } from 'nestjs-pino';
-import { loggerConfig } from './utils/logger-conf';
 import { CorrelationIdMiddleware } from './middlewares/correlation-id.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VendedorModule } from './App-Local-Version-Buena/vendedor/vendedor.module';
 import { ClienteMysqlModule } from './App-Mysql/cliente-mysql/cliente-mysql.module';
-import { CarMysql } from './App-Mysql/entities/car.entity';
-import { ClientMysql } from './App-Mysql/entities/client.entity';
-import { SellerMysql } from './App-Mysql/entities/seller.entity';
 import { VendedorMysqlModule } from './App-Mysql/vendedor-mysql/vendedor-mysql.module';
 import { ClienteModule } from './App-Local-Version-Buena/cliente/cliente.module';
+import { ConfigModule } from '@nestjs/config';
+import { typeOrmAsyncConfig } from './utils/typeorm.config';
+import { loggerConfig } from './utils/logger.config';
 
 @Module({
   imports: [
@@ -22,16 +21,8 @@ import { ClienteModule } from './App-Local-Version-Buena/cliente/cliente.module'
     ClienteMysqlModule, 
     VendedorMysqlModule,
     LoggerModule.forRoot(loggerConfig),
-    TypeOrmModule.forRoot({
-      type: "mariadb",
-      host: "localhost",
-      port: 3306,
-      username: "santi",
-      password: "password11",
-      database: "nestjs_automobiliaria",
-      entities: [SellerMysql, ClientMysql, CarMysql],
-      synchronize: true,
-    }),     
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),    
   ],
   controllers: [],
   providers: [],
